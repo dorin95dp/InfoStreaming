d5002f3b4693510b7d4841b6f430ea9f465dd488
Schidu Vasile <sckeedoo@gmail.com>
2017-06-20 02:04:09 +0300

    resolved commit

--- src/main/java/com/faf/streaming/controllers/MainWindowController.java
+++ src/main/java/com/faf/streaming/controllers/MainWindowController.java
@@ -4,10 +4,13 @@ import com.faf.streaming.utils.MessageSender;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.KeyCode;
+
 
 public class MainWindowController {
     @FXML
@@ -22,6 +25,9 @@ public class MainWindowController {
         if (event.getCode().toString().equals("ENTER")) {
             String userInput = chat.getText();
             chat.clear();
+            Button backspace = new Button("Backspace");
+            KeyEvent press = new KeyEvent(backspace, chat, KeyEvent.KEY_PRESSED, "", "", KeyCode.BACK_SPACE, false, false, false, false);
+            chat.fireEvent(press);
             chatHistory.add(userInput);
             MessageSender.sendMessage(userInput, StartViewController.serverConfig.getIp(),1234);
         }
